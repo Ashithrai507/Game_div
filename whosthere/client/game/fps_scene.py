e9b2c74dc29a61b85a811a4cead132f39c25291e
Ashith Rai <ashithgit@gmail.com>
2026-01-13 20:54:30 +0530

    added passward system

--- whosthere/client/game/fps_scene.py
+++ whosthere/client/game/fps_scene.py
@@ -1,4 +1,4 @@
-from panda3d.core import Vec3
+from panda3d.core import Vec3, CardMaker
 from direct.showbase.DirectObject import DirectObject
 
 class FPSScene(DirectObject):
@@ -6,25 +6,73 @@ class FPSScene(DirectObject):
         self.app = app
         self.app.disableMouse()
 
-        self.speed = 5
-        self.heading = 0
-        self.pitch = 0
+        # --------------------
+        # Player settings
+        # --------------------
+        self.speed = 6.0
+        self.player_height = 1.8
 
-        self.app.taskMgr.add(self.update, "fps-update")
+        # --------------------
+        # Setup world
+        # --------------------
+        self.setup_world()
+        self.setup_camera()
 
+        # Input
+        self.keys = {"w": False, "s": False}
         self.accept("w", self.set_key, ["w", True])
         self.accept("w-up", self.set_key, ["w", False])
-        self.keys = {"w": False}
+        self.accept("s", self.set_key, ["s", True])
+        self.accept("s-up", self.set_key, ["s", False])
 
+        # Update loop
+        self.app.taskMgr.add(self.update, "fps-update")
+
+    # --------------------
+    # World
+    # --------------------
+    def setup_world(self):
+        # Ground
+        cm = CardMaker("ground")
+        cm.setFrame(-50, 50, -50, 50)
+        self.ground = self.app.render.attachNewNode(cm.generate())
+        self.ground.setHpr(0, -90, 0)
+        self.ground.setPos(0, 10, 0)
+        self.ground.setColor(0.15, 0.15, 0.15, 1)
+
+        # Reference cube (so movement is obvious)
+        cube_cm = CardMaker("cube")
+        cube_cm.setFrame(-0.5, 0.5, -0.5, 0.5)
+        self.cube = self.app.render.attachNewNode(cube_cm.generate())
+        self.cube.setPos(0, 15, 1)
+        self.cube.setColor(1, 0, 0, 1)
+
+    # --------------------
+    # Camera
+    # --------------------
+    def setup_camera(self):
+        self.app.camera.reparentTo(self.app.render)
+        self.app.camera.setPos(0, 0, self.player_height)
+
+    # --------------------
+    # Input
+    # --------------------
     def set_key(self, key, value):
         self.keys[key] = value
 
+    # --------------------
+    # Update loop
+    # --------------------
     def update(self, task):
         dt = globalClock.getDt()
 
         if self.keys["w"]:
             self.app.camera.setY(
                 self.app.camera, self.speed * dt
+            )
+        if self.keys["s"]:
+            self.app.camera.setY(
+                self.app.camera, -self.speed * dt
             )
 
         return task.cont
